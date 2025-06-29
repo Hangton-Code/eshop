50b980b8f9cc258facd78ac3d9671b5cb7941e4f
Code Hangton <code.hangton@gmail.com>
2025-06-30 00:25:01 +0800

    make it better

--- actions/stripe.ts
+++ actions/stripe.ts
@@ -56,3 +56,55 @@ export async function fetchClientSecret() {
 
   return session.client_secret;
 }
+
+export async function createStripeSession() {
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
+  const cartItems = await getCartItems();
+
+  if (cartItems.length === 0) {
+    return null;
+  }
+
+  // Create Checkout Sessions from body params.
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+  const session = await stripe.checkout.sessions.create({
+    ui_mode: "hosted",
+    line_items: cartItems.map((item) => ({
+      price_data: {
+        currency: "HKD",
+        product_data: {
+          name: `${item.product.name} from ${item.merchant?.name}`,
+          images: (item.product.covers as Attachment[]).map((_) => _.url),
+          metadata: {
+            productId: item.productId,
+          },
+        },
+        unit_amount: item.product.price * 100,
+      },
+      quantity: item.quantity,
+    })),
+    mode: "payment",
+    currency: "HKD",
+    success_url: `${process.env.NEXT_PUBLIC_APP_ORIGIN}/return?session_id={CHECKOUT_SESSION_ID}`,
+    shipping_address_collection: {
+      allowed_countries: ["HK"],
+    },
+    phone_number_collection: {
+      enabled: true,
+    },
+  });
+
+  await redisClient.set(
+    `stripe:${session.id}`,
+    JSON.stringify({
+      userId,
+      cartItems: cartItems,
+    })
+  );
+
+  return session.url;
+}
--- app/(customer)/checkout/page.tsx
+++ app/(customer)/checkout/page.tsx
@@ -1,9 +1,19 @@
-import Checkout from "@/components/checkout";
+// import Checkout from "@/components/checkout";
 
-export default function CheckoutPage() {
-  return (
-    <div id="checkout">
-      <Checkout />
-    </div>
-  );
+import { createStripeSession } from "@/actions/stripe";
+import { redirect } from "next/navigation";
+
+// export default function CheckoutPage() {
+//   return (
+//     <div id="checkout">
+//       <Checkout />
+//     </div>
+//   );
+// }
+
+export default async function CheckoutPage() {
+  const sessionUrl = await createStripeSession();
+  if (!sessionUrl) redirect("/");
+
+  redirect(sessionUrl);
 }
